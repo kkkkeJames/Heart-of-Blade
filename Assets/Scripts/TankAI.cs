8494ad10666058beed66bf7daedb125e97cd7d11
Lumgol <88503543+Lumgol@users.noreply.github.com>
2024-02-16 21:06:09 -0500

    fixed compilation errors & basic movement seems to be working ok i think?

--- Assets/Scripts/TankAI.cs
+++ Assets/Scripts/TankAI.cs
@@ -15,6 +15,11 @@ public class TankAI : Enemy
     [SerializeField] private float dashCooldown = 2f;
     [SerializeField] private float dashSpeed = 2f;
 
+    private Vector2 baseSpeed = new Vector2(6f, 0f);
+    private float currDashCooldown;
+    private float currDashDuration = 0f;
+    private bool isDashing;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +28,7 @@ public class TankAI : Enemy
         myFeet = GetComponent<BoxCollider2D>();
         myCapsule = GetComponent<CapsuleCollider2D>();
         health = 500;
-        private float baseSpeed = new Vector2(6f, 0f);
-        private float currDashCooldown = dashCooldown;
-        private float currDashDuration = 0f;
-        private bool isDashing;
+        currDashCooldown = dashCooldown;
     }
 
     // Update is called once per frame
@@ -36,30 +38,30 @@ public class TankAI : Enemy
 
         if (currDashCooldown <= 0) {
             isDashing = true;
+            rb.velocity = new Vector2(dashSpeed * rb.velocity.x, baseSpeed.y);
             currDashCooldown = dashCooldown;
         }
 
         if (currDashDuration <= 0) {
             isDashing = false;
             currDashDuration = dashDuration;
+            if (playerTransform.position.x >= transform.position.x) {
+                rb.velocity = baseSpeed;
+            } else rb.velocity = new Vector2(-baseSpeed.x, baseSpeed.y);
         }
 
         // daaaaaaaash
         if (isDashing) {
-            rb.velocity = (dashSpeed * baseSpeed.x, baseSpeed.y);
             currDashDuration -= Time.deltaTime;
         }
 
         // switch direction to follow player iff not dashing
         else {
-            if (playerTransform.position.x >= transform.position.x) {
-                rb.velocity = baseSpeed;
-            } else rb.velocity = (-baseSpeed.x, baseSpeed.y);
 
             currDashCooldown -= Time.deltaTime;
         } 
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -67,5 +69,4 @@ public class TankAI : Enemy
             collision.GetComponent<PlayerMovement>().GetHit(10, 0.2f);
         }
     }
-
 }
