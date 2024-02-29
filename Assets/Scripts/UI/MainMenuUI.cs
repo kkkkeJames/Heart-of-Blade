af676b2a3eb0b02b9d3d0bcee0ab387a753d17eb
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2024-02-28 22:46:46 -0500

    options menu

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using Application = UnityEngine.Device.Application;
 
 public class MainMenu : UI
 {
     public static MainMenu Instance { get; private set; }
     
     [SerializeField] private ButtonUI newGameButton;
-    [SerializeField] private ButtonUI continueGameButton;
-    [SerializeField] private ButtonUI loadGameButton;
     [SerializeField] private ButtonUI quitGameButton;
     [SerializeField] private ButtonUI settingsButton;
 
@@ -22,8 +17,6 @@ public class MainMenu : UI
     private void Start() 
     {
         newGameButton.AddListener(OnNewGameClicked);
-        continueGameButton.AddListener(OnContinueGameClicked);
-        loadGameButton.AddListener(OnLoadGameClicked);
         quitGameButton.AddListener(OnQuitGameClicked);
         settingsButton.AddListener(OnSettingsClicked);
         
@@ -35,16 +28,12 @@ public class MainMenu : UI
         if (enable)
         {
             newGameButton.Enable();
-            continueGameButton.Enable();
-            loadGameButton.Enable();
             quitGameButton.Enable();
             settingsButton.Enable();
         }
         else
         {
             newGameButton.Disable();
-            continueGameButton.Disable();
-            loadGameButton.Disable();
             quitGameButton.Disable();
             settingsButton.Disable();
         }
@@ -56,21 +45,7 @@ public class MainMenu : UI
         Debug.Log("NEW GAME is not currently implemented but here we go...");
         Loader.Load("MainScene");
     }
-
-    private void OnContinueGameClicked()
-    {
-        ToggleButtons(false);
-        Debug.Log("CONTINUE");
-        Loader.Load("MainScene");
-    }
     
-    private void OnLoadGameClicked() 
-    {
-        Hide();
-        Debug.LogError("LOAD GAME is not currently implemented!");
-        // SaveSlotsMenu.Instance.DisplayLoadGameMenu();
-    }
-
     private void OnQuitGameClicked()
     {
         Hide();
@@ -83,7 +58,7 @@ public class MainMenu : UI
         });
     }
 
-    protected override void Deactivate()
+    protected override void LateDeactivate()
     {
         ToggleButtons(false);
     }
@@ -91,17 +66,11 @@ public class MainMenu : UI
     private void OnSettingsClicked()
     {
         Hide();
-        Debug.LogError("SETTINGS is not currently implemented!");
-        // OptionsMainMenu.Instance.Show();
+        OptionsMenu.Instance.Show();
     }
     
     protected override void Activate()
     {
         ToggleButtons(true);
-        // if (!DataPersistenceManager.Instance.HasGameData()) 
-        // {
-        //     continueGameButton.Disable();
-        //     loadGameButton.Disable();
-        // }
     }
 }
