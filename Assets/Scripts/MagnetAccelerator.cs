8442739d8ab5ed9130eb77e36b4a6648a9370d11
Lumgol <88503543+Lumgol@users.noreply.github.com>
2024-03-10 14:09:40 -0400

    stuff

--- Assets/Scripts/MagnetAccelerator.cs
+++ Assets/Scripts/MagnetAccelerator.cs
@@ -7,6 +7,9 @@ public class MagnetAccelerator : MonoBehaviour
 {
     private CapsuleCollider2D magnetAccelerator;
     [SerializeField] private Vector2 lingerSpeed;
+    [SerializeField] private float maxBoostRadius = 0.5f;
+    [SerializeField] private float minBoostRadius = 2f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +21,20 @@ public class MagnetAccelerator : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (Input.GetButton("Interact"))
-            {
-                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().lingerSpeed = lingerSpeed;
-                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().lingerSpeedTime = 0.2f;
-            }
+            var player = GameObject.FindGameObjectWithTag("Player");
+            Transform playerTransform = player.GetComponent<Transform>().transform;
+            Vector2 tilePosition = magnetAccelerator.offset;// tilemap.CellToWorld(cellPosition);
+            Vector2 playerxy = new Vector2(playerTransform.position.x, playerTransform.position.y);
+            float playerDistance = (playerxy - tilePosition).magnitude;
+
+            Vector2 playerSpeedBoost = lingerSpeed * Mathf.Min(Mathf.Max(minBoostRadius - playerDistance, 0f) / (minBoostRadius - maxBoostRadius), 1f);
+            Debug.Log(playerDistance + ", " + playerSpeedBoost);
+            player.GetComponent<PlayerController>().lingerSpeed = playerSpeedBoost;
+            player.GetComponent<PlayerController>().lingerSpeedTime = 0.2f;
         }
     }
 }
